5b286b7f943a4c04f7c0945d7d13642abcbb30a4
Morgan <murgntheurgn@gmail.com>
2026-01-04 22:47:43 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-const date = new Date(document.lastModified);
+const element = document.getElementById("last-updated");
+if(element)
+{
+    const date = new Date(document.lastModified);
 
-const day = String(date.getDate()).padStart(2, "0");
-const month = String(date.getMonth() + 1).padStart(2, "0");
-const year = date.getFullYear();
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
 
-const formatted = `Last Updated: ${day}/${month}/${year}`;
-document.getElementById("last-updated").textContent = formatted;
+    const formatted = `Last Updated: ${day}/${month}/${year}`;
+    element.textContent = formatted;
+}
 
 var buttons = document.querySelectorAll(".collapsible button");
 
